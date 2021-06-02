e9f3321575f92433c6b59767b345c4c84a18bc2b
MarwanAl2 <>
2021-06-02 13:53:40 +0000

    finshing

---
+++ java.js
@@ -0,0 +1,8 @@
+// hello every one who can see this code it is just my first attempt 
+alert ('are you sure to continue?' )
+var yourage = prompt('write your age to cheak if you are allowed to enter')
+if(yourage >= 18) {alert('you can continue')}
+
+else{alert('you have to leave this page') }
+var name =prompt('write you sur name please')
+document.write ('<h4>' + name + '</h4>' )
