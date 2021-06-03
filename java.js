e4d9ee39b8e8795824a0917fd3ab7adfa7060022
MarwanAl2 <>
2021-06-03 15:07:32 +0000

    addcss

--- java.js
+++ java.js
@@ -1,31 +1,31 @@
-// hello every one who can see this code it is just my first attempt 
-alert ('are you sure to continue?' )
-var yourage = prompt('write your age to cheak if you are allowed to enter')
+// // hello every one who can see this code it is just my first attempt 
+// alert ('are you sure to continue?' )
+// var yourage = prompt('write your age to cheak if you are allowed to enter')
 
 
 
 
-while(yourage<18)
-{
-  alert('you are so young we can`t allow for you to access this page ')
-  var yourage = prompt('write your age to cheak if you are allowed to enter')
+// while(yourage<18)
+// {
+//   alert('you are so young we can`t allow for you to access this page ')
+//   var yourage = prompt('write your age to cheak if you are allowed to enter')
 
   
-}
-var name =prompt('write you sur name please')
-var star=prompt("What star rating would you give from 1-5")
-while(star>5 || star <=0)
-{ 
-  alert('please you have to chose number from 1-5 ')
- var star=prompt("What star rating would you give from 1-5")
+// }
+// var name =prompt('write you sur name please')
+// var star=prompt("What star rating would you give from 1-5")
+// while(star>5 || star <=0)
+// { 
+//   alert('please you have to chose number from 1-5 ')
+//  var star=prompt("What star rating would you give from 1-5")
 
-}
+// }
 
-for ( i=0;star>i ; i++)
-{
+// for ( i=0;star>i ; i++)
+// {
 
-document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" width="100" height="100">')
+// document.write('<img src="https://image.pngaaa.com/369/1372369-middle.png" width="100" height="100">')
 
-}
-alert('you can continue')
-document.write ('<h4>' + name + '</h4>' )
+// }
+// alert('you can continue')
+// document.write ('<h4>' + name + '</h4>' )
